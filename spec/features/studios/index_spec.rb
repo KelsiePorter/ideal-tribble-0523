db0f7f691a4f40f1b1b4defa97dbe54c8ee279cf
Kelsie Porter <krporter22@gmail.com>
2023-01-02 10:06:13 -0700

    WIP: user story 1

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe 'studios index page' do
     @studio_1 = Studio.create!(name: 'Universal Studios', location: '123 Hollywood Blvd')
     @studio_2 = Studio.create!(name: 'Warner Brothers', location: '555 Santa Monica Blvd')
 
-    @movie_1 = Movie.create!(title: 'Bring It On', creation_year: '2000', genre: 'Comedy', @studio_1.id)
-    @movie_2 = Movie.create!(title: 'Legally Blond', creation_year: '2001', genre: 'Documentary', @studio_1.id)
-    @movie_3 = Movie.create!(title: 'Chicago', creation_year: '2002', genre: 'Drama', @studio_1.id)
-    @movie_4 = Movie.create!(title: 'Under the Tuscan Sun', creation_year: '2003', genre: 'Romance', @studio_2.id)
-    @movie_5 = Movie.create!(title: 'Mean Girls', creation_year: '2004', genre: 'Comedy', @studio_2.id)
+    @movie_1 = Movie.create!(title: 'Bring It On', creation_year: '2000', genre: 'Comedy', studio_id: @studio_1.id)
+    @movie_2 = Movie.create!(title: 'Legally Blond', creation_year: '2001', genre: 'Documentary', studio_id: @studio_1.id)
+    @movie_3 = Movie.create!(title: 'Chicago', creation_year: '2002', genre: 'Drama', studio_id: @studio_1.id)
+
+    @movie_4 = Movie.create!(title: 'Under the Tuscan Sun', creation_year: '2003', genre: 'Romance', studio_id: @studio_2.id)
+    @movie_5 = Movie.create!(title: 'Mean Girls', creation_year: '2004', genre: 'Comedy', studio_id: @studio_2.id)
 
     @actor_1 = Actor.create!(name: 'Kirsten Dunst', age: 20)
     @actor_2 = Actor.create!(name: 'Reese Witherspoon', age: 20)
@@ -32,11 +33,44 @@ RSpec.describe 'studios index page' do
     @movie_5.actors << @actor_6
 
   end
+  #US1
   it 'displays all studios and their attributes' do 
+    visit '/studios'
 
+    within("section#studio-#{@studio_1.id}") do
+      expect(page).to have_content(@studio_1.name)
+      expect(page).to have_content(@studio_1.location)
+    end
+    
+    within("section#studio-#{@studio_2.id}") do
+      expect(page).to have_content(@studio_2.name)
+      expect(page).to have_content(@studio_2.location)
+    end
   end
   #US1
-  xit 'for each studio I see all of the studios movies and the movies attributes' do 
+  it 'for each studio I see all of the studios movies and the movies attributes' do 
+    visit '/studios'
+    # save_and_open_page
 
+    within("section#studio-#{@studio_1.id}") do
+      expect(page).to have_content(@movie_1.title)
+      expect(page).to have_content(@movie_1.creation_year)
+      expect(page).to have_content(@movie_1.genre)
+      expect(page).to have_content(@movie_2.title)
+      expect(page).to have_content(@movie_2.creation_year)
+      expect(page).to have_content(@movie_2.genre)
+      expect(page).to have_content(@movie_3.title)
+      expect(page).to have_content(@movie_3.creation_year)
+      expect(page).to have_content(@movie_3.genre)
+    end
+
+    within("section#studio-#{@studio_2.id}") do
+      expect(page).to have_content(@movie_4.title)
+      expect(page).to have_content(@movie_4.creation_year)
+      expect(page).to have_content(@movie_4.genre)
+      expect(page).to have_content(@movie_5.title)
+      expect(page).to have_content(@movie_5.creation_year)
+      expect(page).to have_content(@movie_5.genre)
+    end
   end
 end
