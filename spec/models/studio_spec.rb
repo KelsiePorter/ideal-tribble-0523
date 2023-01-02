d9c168f68ab96e95b5ccf770254d671232e87c53
Kelsie Porter <krporter22@gmail.com>
2023-01-02 10:56:24 -0700

    Update instance method test

--- spec/models/studio_spec.rb
+++ spec/models/studio_spec.rb
@@ -17,11 +17,11 @@ RSpec.describe Studio do
       @movie_4 = Movie.create!(title: 'Under the Tuscan Sun', creation_year: '2003', genre: 'Romance', studio_id: @studio_2.id)
       @movie_5 = Movie.create!(title: 'Mean Girls', creation_year: '2004', genre: 'Comedy', studio_id: @studio_2.id)
     end
+    
     describe '#studio_movie' do 
       it 'returns all movies associated with a studio' do 
-        expect(@studio_1.studio_movies.count).to eq(3)
-        expect(@studio_2.studio_movies.count).to eq(2)
-        #not sure how to thoroughly test since my method returns an AR association
+        expect(@studio_1.studio_movies).to eq([@movie_1, @movie_2, @movie_3])
+        expect(@studio_2.studio_movies).to eq([@movie_4, @movie_5])
       end
     end
   end
