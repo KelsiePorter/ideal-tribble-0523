1791498e588bb879e3f3babe5a6c12b1d4ffc5d2
Kelsie Porter <krporter22@gmail.com>
2023-01-02 11:16:21 -0700

    Fix: before each section

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -8,19 +8,19 @@ RSpec.describe Movie do
   end
 
   describe 'instance methods' do 
-    describe '#youngest_oldest_actors' do 
-      before :each do 
-        @studio_1 = Studio.create!(name: 'Universal Studios', location: '123 Hollywood Blvd')
-        @movie_1 = Movie.create!(title: 'Bring It On', creation_year: '2000', genre: 'Comedy', studio_id: @studio_1.id)
-        
-        @actor_1 = Actor.create!(name: 'Kirsten Dunst', age: 20)
-        @actor_5 = Actor.create!(name: 'Sandra Oh', age: 40)
-        @actor_6 = Actor.create!(name: 'Lindsey Lohan', age: 66)
+    before :each do 
+      @studio_1 = Studio.create!(name: 'Universal Studios', location: '123 Hollywood Blvd')
+      @movie_1 = Movie.create!(title: 'Bring It On', creation_year: '2000', genre: 'Comedy', studio_id: @studio_1.id)
+      
+      @actor_1 = Actor.create!(name: 'Kirsten Dunst', age: 20)
+      @actor_5 = Actor.create!(name: 'Sandra Oh', age: 40)
+      @actor_6 = Actor.create!(name: 'Lindsey Lohan', age: 66)
 
-        @movie_1.actors << @actor_1
-        @movie_1.actors << @actor_5
-        @movie_1.actors << @actor_6
-      end
+      @movie_1.actors << @actor_1
+      @movie_1.actors << @actor_5
+      @movie_1.actors << @actor_6
+    end
+    describe '#youngest_oldest_actors' do 
       it 'returns actors in order from youngest to oldest' do 
 
         expect(@movie_1.youngest_oldest_actors).to eq([@actor_1, @actor_5, @actor_6])
